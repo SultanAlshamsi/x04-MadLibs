2b15644c131197f22f8e0088a28e5a034d049ef0
ssalshamsi <ssalshamsi@mail.csuchico.edu>
2018-02-01 15:45:42 -0800

    Describe what is new in your own words

--- madlibs.cpp
+++ madlibs.cpp
@@ -5,5 +5,41 @@
 int main()
 {
 
+std::string name;
+
+int model;
+
+double Price;
+
+int Horsepower; 
+/////////////
+
+std::cout<< "Enter a name" << std::endl; 
+
+std::cin >> name;
+
+std::cout << "Enter a Car model"<< std::endl;
+
+std::cin >> model;
+
+std::cout << "Enter a Car Price in dollars "<< std::endl;
+
+std::cin >> Price;
+
+std::cout << "Enter a Car Horsepower unit " << std::endl;
+
+std::cin >> Horsepower;
+
+///////////////
+
+
+std::cout << " On 21st of January " << name << " bought a brand new car. " <<std::endl;  
+
+std::cout << " it's a Nice looking car its model " << model <<std::endl;
+
+std::cout << "He say that he bought it for " << Price << " $  ,i think it's cheap " << std::endl;
+
+std::cout << " the car has " << Horsepower << " HP. This is an insane power !! " << std::endl;
+
   return 0;
 }
